08f79813f4933b5c109fa83edff88e4c2c12588d
KIHALAya <kihalaya00@gmail.com>
2025-01-12 14:09:48 +0100

    adjust the  Client  class to remove the dependency on Utilisateur

--- app/src/main/java/Model/Metier/Client.java
+++ app/src/main/java/Model/Metier/Client.java
@@ -1,53 +1,92 @@
 package main.java.Model.Metier;
 
-
 import java.util.ArrayList;
 
-public class Client extends Utilisateur{
-    private Panier panier ;
-    private ArrayList<Commande> Commandes ;
-    private String AdresseLivraison ;
+public class Client {
+    // Attributes
+    private int ID;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse;
+    private String tele;
+    private Panier panier;
+    private ArrayList<Commande> Commandes;
+    private String AdresseLivraison;
 
-
-
-
-    public Client(int ID, String nom,String prenom,String email,String motDePasse,String tele,Panier panier,ArrayList<Commande> Commandes,String AdresseLivraison){
-        super(ID ,nom,prenom,email,motDePasse,tele);
-        this.panier=panier;
-        this.Commandes=new ArrayList<>();
-        this.AdresseLivraison=AdresseLivraison;
-
-    }
-    public Client(){
-        super();
-        this,panier=panier;
-        this.Commandes= new ArrayList<>();
-        this.AdresseLivraison=AdresseLivraison;
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "panier=" + panier +
-                ", Commandes=" + Commandes +
-                ", AdresseLivraison='" + AdresseLivraison + '\'' +
-                '}';
+    // Constructor with parameters
+    public Client(int ID, String nom, String prenom, String email, String motDePasse, String tele, Panier panier, ArrayList<Commande> Commandes, String AdresseLivraison) {
+        this.ID = ID;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.tele = tele;
+        this.panier = panier;
+        this.Commandes = Commandes != null ? Commandes : new ArrayList<>();
+        this.AdresseLivraison = AdresseLivraison;
     }
 
-    public String getAdresseLivraison() {
-        return AdresseLivraison;
+    // Default constructor
+    public Client() {
+        this.ID = 0;
+        this.nom = "";
+        this.prenom = "";
+        this.email = "";
+        this.motDePasse = "";
+        this.tele = "";
+        this.panier = new Panier(); // Assuming Panier has a default constructor
+        this.Commandes = new ArrayList<>();
+        this.AdresseLivraison = "";
     }
 
-    public void setAdresseLivraison(String adresseLivraison) {
-        AdresseLivraison = adresseLivraison;
+    // Getters and Setters
+    public int getID() {
+        return ID;
     }
 
-    public ArrayList<Commande> getCommandes() {
-        return Commandes;
+    public void setID(int ID) {
+        this.ID = ID;
     }
 
-    public void setCommandes(ArrayList<Commande> commandes) {
-        Commandes = commandes;
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public String getTele() {
+        return tele;
+    }
+
+    public void setTele(String tele) {
+        this.tele = tele;
     }
 
     public Panier getPanier() {
@@ -56,5 +95,37 @@ public class Client extends Utilisateur{
 
     public void setPanier(Panier panier) {
         this.panier = panier;
+    }
+
+    public ArrayList<Commande> getCommandes() {
+        return Commandes;
+    }
+
+    public void setCommandes(ArrayList<Commande> Commandes) {
+        this.Commandes = Commandes;
+    }
+
+    public String getAdresseLivraison() {
+        return AdresseLivraison;
+    }
+
+    public void setAdresseLivraison(String AdresseLivraison) {
+        this.AdresseLivraison = AdresseLivraison;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Client{" +
+                "ID=" + ID +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", motDePasse='" + motDePasse + '\'' +
+                ", tele='" + tele + '\'' +
+                ", panier=" + panier +
+                ", Commandes=" + Commandes +
+                ", AdresseLivraison='" + AdresseLivraison + '\'' +
+                '}';
     }
 }
